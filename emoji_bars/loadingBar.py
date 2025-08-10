8c1affcd949c2af8fdce71ef0d6dceb05b5f8e93
Mathdallas-code <jaimorampudi164@gmail.com>
2025-08-10 13:06:31 +0530

    Organized files

--- emoji_bars/loadingBar.py
+++ emoji_bars/loadingBar.py
@@ -1,10 +1,13 @@
 # This contains the main code
-import time
 
+# These are just for the nice terminal colour
 GREEN = "\033[32m"
 RED = "\033[31m"
 
 RESET = "\033[0m"  # Resets to default terminal color
+
+import warnings
+import time
 
 
 class LoadingBar:
@@ -16,11 +19,18 @@ class LoadingBar:
         self.capacity = capacity
         self.isPercentage = isPercentage
 
-    def print_bar(self, value, end=False):
-
+    def print_bar(
+        self,
+        value: int,
+        prefix: str,
+        suffix: str = "",
+        display_status=True,
+        end=False,
+    ):
         output = ""
+        status = ""
         if value > self.capacity:
-            return "Error: value must be smaller than or equal to total capacity"
+            warnings.warn("value must be smaller than total capacity")
         else:
             for i in range(0, self.capacity):
                 if i < value:
@@ -29,33 +39,33 @@ class LoadingBar:
                     output = output + self.off_emoji
 
         if self.isPercentage:
+            status = round(value / self.capacity * 100)
             if not end:
                 print(
-                    f"{output} {GREEN if value == self.capacity else RED} {round(value / self.capacity * 100)}% {RESET}",
+                    f"{prefix} {GREEN} {output} {suffix} {GREEN if value == self.capacity else RED} {status if display_status else ''}% {RESET}",
                     end="\r",
                 )
             else:
                 print(
-                    f"{output} {GREEN if value == self.capacity else RED} {round(value / self.capacity * 100)}% {RESET}",
+                    f"{prefix} {GREEN} {output} {suffix} {GREEN if value == self.capacity else RED} {status if display_status else ''}% {RESET}",
                     end="\n",
                 )
         else:
+            status = str(value) + "/" + str(self.capacity)
             if not end:
                 print(
-                    f"{output} {GREEN if value == self.capacity else RED} {value}/{self.capacity} {RESET}",
+                    f"{prefix} {GREEN} {output} {suffix} {GREEN if value == self.capacity else RED} {status if display_status else ''} {RESET}",
                     end="\r",
                 )
             else:
                 print(
-                    f"{output} {GREEN if value == self.capacity else RED} {value}/{self.capacity} {RESET}",
+                    f"{prefix} {GREEN} {output} {suffix} {GREEN if value == self.capacity else RED} {status if display_status else ''} {RESET}",
                     end="\n",
                 )
 
 
-# Basic tests
 if __name__ == "__main__":
-    testBar = LoadingBar("🟩", "⬜️", 10)
-
+    testBar = LoadingBar("█", "▒", 10)
     for i in range(0, testBar.capacity + 1):
-        testBar.print_bar(i, False if i < testBar.capacity else True)
+        testBar.print_bar(i, "Loading:")
         time.sleep(0.5)
